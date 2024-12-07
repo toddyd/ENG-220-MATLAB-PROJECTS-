1cc4e8e65f5e2e39ddbb6b827de19dd94d1dcb37
Devin Toddy <142960495+toddyd@users.noreply.github.com>
2024-12-07 12:34:42 -0700

    Update and rename untitled2.py to projectcodeDT.py

--- projectcodeDT.py
+++ projectcodeDT.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 st.title("CSV Data Visualization App")
 
 # File uploader for CSV
-uploaded_file = st.file_uploader(, type=["csv"])
+uploaded_file = st.file_uploader("ratings.csv", type=["csv"])
 
 if uploaded_file is not None:
     # Read the CSV file
